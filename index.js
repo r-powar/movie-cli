9c4ecbcdc3b9e20187ed33d50ce0e209523fd581
Raj Powar <raaj.powar@gmail.com>
2016-04-10 17:24:16 -0700

    help module

--- index.js
+++ index.js
@@ -3,54 +3,24 @@
  * Created by rpowar on 1/25/16.
  */
 
-//api key = cc9593683bd74287b82d30c78f49fe34
 //core
-var ProgressBar = require('progress');
-var http = require('https');
+var yargs = require('yargs').argv;
+var chalk = require('chalk');
+
+//file scope
+var Config = require('./lib/config');
 
 //constants
-var apiKey = "cc9593683bd74287b82d30c78f49fe34";
-var basepath = "https://api.themoviedb.org/3";
+var configConstants = require('./config/config_constants');
 
-//filescope
-var configurationUrl = basepath + "/configuration";
-var initialRequest = configurationUrl + "?api_key=" + apiKey;
+var configurationUrl = configConstants.basepath + "movie/now_playing";
+var latestMovies = configurationUrl + "?api_key=" + configConstants.apiKey;
 
-var request = http.get(initialRequest);
+var parameters ={
+    args: process.argv
+};
 
-request.on('response', function (response) {
-    var body = "";
-    var len = parseInt(response.headers['content-length'], 10);
-    var bar = new ProgressBar(' downloading [:bar] :percent :etas', {
-        complete: '=',
-        incomplete: ' ',
-        width: 20,
-        total: len
-    });
+console.log('\t' + '\t' +chalk.red.bold('MovieDB'));
 
-    response.on('data', function (chunk) {
-        body += chunk;
-        bar.tick(body.length);
-    });
-
-    response.on('end', function () {
-        console.log('\nFinished Loading\n');
-    });
-
-});
-
-request.end();
-//request(initialRequest, function(error, response, body){
-//    if(!error && response.statusCode === 200){
-//
-//    }
-//});
-
-
-//var timer = setInterval(function () {
-//    bar.tick();
-//    if (bar.complete) {
-//        console.log('\ndone\n');
-//        clearInterval(timer);
-//    }
-//}, 100);
+var config = new Config(parameters);
+config.start();
