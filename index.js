3d8e187c1f2598e3e1495e5913c7347d0a5c42b2
Raj Powar <raaj.powar@gmail.com>
2016-05-09 00:10:26 -0700

    initial table integration

--- index.js
+++ index.js
@@ -4,17 +4,10 @@
  */
 
 //core
-var yargs = require('yargs').argv;
 var chalk = require('chalk');
 
 //file scope
 var Config = require('./lib/config');
-
-//constants
-var configConstants = require('./config/config_constants');
-
-var configurationUrl = configConstants.basepath + "movie/now_playing";
-var latestMovies = configurationUrl + "?api_key=" + configConstants.apiKey;
 
 var parameters ={
     args: process.argv
